fd5586cced25e8ed6b98ac46145d363bfaea2f2b
wipermail <wipermail@gmail.com>
2017-05-13 22:30:42 +0300

    update mail

--- contact/form-handler.php
+++ contact/form-handler.php
@@ -2,7 +2,7 @@
 include('SMTPClass.php');
 
 $use_smtp = '0';
-$emailto = 'yourname@yourdomain.com';
+$emailto = 'wipermail@gmail.com';
 
 	// retrieve from parameters
 	$emailfrom = isset($_POST["email"]) ? $_POST["email"] : "";
